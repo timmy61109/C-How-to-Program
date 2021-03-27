c8df4bac33ab4ffb73425056e2e08d6fe4eb504f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-03-28 02:22:39 +0800

    如果採用，此提交將提交第一版本習題3.16作業
    修改項目:
    
    這是目前有問題，會出現以下問題。
    
    [C] format %s expects type char *, but argument 2 has type int
    
    模組:
    
    議題
    合併請求

---
+++ business_tax.c
@@ -0,0 +1,50 @@
+// business_tax.c
+/*
+營業稅
+
+營業稅是向買方收取並付給政府的，零售商必須提交每個月營業稅報表，其中列出該月的營業額合所徵收的營
+業稅額合徵收的營業稅額，並分別列出郡政府和州政府稅。請開發一個程式，輸入一個月的總收入，計算收入
+的營業稅，並分別顯示出郡政府稅和政府稅。假設週的營業稅率是4%，郡的營業稅是5%。以下是輸入／輸出
+的對話範例。
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  float total;
+  char name;
+  float sales;
+  float county_sales_tax;
+  float state_sales_tax;
+  float total_sales_tax;
+
+  while (total != -1) {
+    total = 0;
+    sales = 0;
+    county_sales_tax = 0;
+    state_sales_tax = 0;
+    total_sales_tax = 0;
+
+    printf("Enter total amount collected (-1 to quit):");
+    scanf("%f", &total);
+
+    if (total != -1) {
+      printf("Enter name of month: ");
+      scanf("%s", &name);
+
+      county_sales_tax = total * 0.04;
+      state_sales_tax = total * 0.05;
+      total_sales_tax = county_sales_tax + state_sales_tax;
+      sales = total - total_sales_tax;
+
+      printf("Total Collections: $%.0f\n", total);
+      printf("Sales: $%.3f\n", sales);
+      printf("County Sales Tax: $%.3f\n", county_sales_tax);
+      printf("State Sales Tax: $%.3f\n", state_sales_tax);
+      printf("Total Sales Tax: $%.3f\n\n", total_sales_tax);
+
+    }
+
+  }
+}
