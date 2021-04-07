ee98db007a366bf0aaf27c58c6ffd58fa11fdeaf
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-04-07 11:03:35 +0800

    如果採用，此提交將完成第三次作業
    修改項目:
    
    模組:
    
    議題
    合併請求

--- test.c
+++ test.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-  int x;
+  int n;
+  int sum;
+  int square;
+  int cube;
+  int count;
 
-  for (x = 15; x <= 55; x += 8){
-    printf("%d\n", x);
+  sum = 0;
+  square = 0;
+  cube = 0;
+
+  printf("%s", "輸入一個非負號的整數：");
+  scanf("%d", &n);
+  printf("%s\t%s\t%s\n", "總和", "平方和", "立方和");
+  for (count = 1; count <= n; count++) {
+    sum += count;
+    square += count * count;
+    cube += count * count * count;
   }
+  printf("%d\t%d\t%d\n", sum, square, cube);
 }
