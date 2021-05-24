34c3c782325049535132070afe71de2bcce8919f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-05-24 19:13:32 +0800

    如果採用，此提交將做出有驗證的方式得知程式的設計是否正確
    修改項目:
    
    模組:
    
    議題

--- test.c
+++ test.c
@@ -5,32 +5,43 @@
 
 
 int main(void) {
-  int cuboid[36];
+  int cuboid[360];
+  int count[13];
   int throw1;
   int throw2;
   srand(time(NULL));
 
-  for (size_t i = 0; i < 36; ++i) {
+  for (size_t i = 0; i < 360; ++i) {
       cuboid[i] = 0;
   }
 
+  for (size_t i = 0; i < 13; ++i) {
+      count[i] = 0;
+  }
+
   for (size_t i = 0; i < 36000; ++i) {
-    throw1 = rand() % SIZE;
-    throw2 = rand() % SIZE;
-    cuboid[throw1 * throw2 - 1] += 1;
+    throw1 = rand() % SIZE + 1;
+    throw2 = rand() % SIZE + 1;
+    printf("%d, %d\n", throw1, throw2);
+    cuboid[throw1 + throw2 * 10] += 1;
+    count[throw1 + throw2] += 1;
   }
 
   printf("     ");
-  for (size_t i = 0; i < SIZE; ++i) {
-    printf("%5lu", i);
+  for (size_t i = 1; i <= SIZE; ++i) {
+    printf("%6lu", i);
   }
   printf("\n");
 
-  for (size_t i = 0; i < SIZE; ++i) {
-    printf("%5lu", i);
-    for (size_t j = 0; j < SIZE; ++j) {
-      printf("%5d", cuboid[i * j]);
+  for (size_t i = 1; i <= SIZE; ++i) {
+    printf("%6lu", i);
+    for (size_t j = 1; j <= SIZE; ++j) {
+      printf("%6d", cuboid[i + j * 10]);
     }
     printf("\n");
   }
+  for (size_t i = 1; i < 13; ++i) {
+    printf("%d ", count[i]);
+  }
+  puts("");
 }
