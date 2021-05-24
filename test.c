70f6e999ab59b1180bff2eda730cc72d2dfd3af4
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-05-24 19:38:27 +0800

    如果採用，此提交將更新表達方式
    修改項目:
    
    模組:
    
    議題

--- test.c
+++ test.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 #define SIZE 6
+#define COUNT_SIZE 12
+#define CUBOID_SIZE 360
 
 
 int main(void) {
-  int cuboid[360];
-  int count[13];
+  int cuboid[CUBOID_SIZE];
+  int count[COUNT_SIZE];
   int throw1;
   int throw2;
   srand(time(NULL));
 
-  for (size_t i = 0; i < 360; ++i) {
+  for (size_t i = 0; i < CUBOID_SIZE; ++i) {
       cuboid[i] = 0;
   }
 
-  for (size_t i = 0; i < 13; ++i) {
+  for (size_t i = 0; i < COUNT_SIZE; ++i) {
       count[i] = 0;
   }
 
   for (size_t i = 0; i < 36000; ++i) {
     throw1 = rand() % SIZE + 1;
     throw2 = rand() % SIZE + 1;
-    printf("%d, %d\n", throw1, throw2);
     cuboid[throw1 + throw2 * 10] += 1;
-    count[throw1 + throw2] += 1;
+    count[throw1 + throw2 - 1] += 1;
   }
 
   printf("     ");
@@ -40,7 +41,9 @@ int main(void) {
     }
     printf("\n");
   }
-  for (size_t i = 1; i < 13; ++i) {
+  puts("");
+
+  for (size_t i = 0; i < COUNT_SIZE; ++i) {
     printf("%d ", count[i]);
   }
   puts("");
