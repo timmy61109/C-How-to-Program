4475f74681f26f489e6ec9318386b6bb0fc1878b
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-05-24 16:54:05 +0800

    如果採用，此提交將更新作業
    修改項目:
    
    模組:
    
    議題

--- test.c
+++ test.c
@@ -1,51 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define SIZE_1 5
-#define SIZE_2 20
+#define SIZE 900
 
 
-int main(void) {
-  int grades[SIZE_1][SIZE_2];
-  double mathGrades[SIZE_2];
-  srand(time(NULL));
+int main(int argc, char const *argv[]) {
+  int reward[100];
 
-  for (size_t i = 0; i < SIZE_1; ++i) {
-    for (size_t j = 0; j < SIZE_2; ++j) {
-      grades[i][j] = rand() % 100;
-    }
+  for (size_t i = 0; i < 100; ++i) {
+    reward[i] = 200 + ((SIZE + i) * 0.09);
   }
-  for (size_t i = 0; i < SIZE_2; ++i) {
-    printf("%4lu", i);
-  }
-  printf("\n");
-  for (size_t i = 0; i < SIZE_1; ++i) {
-    printf("%4lu", i);
-    for (size_t j = 0; j < SIZE_2; ++j) {
-      printf("%4d", grades[i][j]);
-    }
-    printf("\n");
-  }
-
-  printf("\n\n    ");
-  for (size_t j = 0; j < SIZE_2; ++j) {
-    mathGrades[j] = rand() % 100;
-    printf("%4.0f", mathGrades[j]);
-  }
-
-  printf("\n\n    ");
-  for (size_t i = 0; i < SIZE_2; ++i) {
-    printf("%4lu", i);
-  }
-  printf("\n");
-  for (size_t i = 0; i < SIZE_1; ++i) {
-    printf("%4lu", i);
-    for (size_t j = 0; j < SIZE_2; ++j) {
-      if (i == 0) {
-        grades[i][j] = mathGrades[j];
-      }
-      printf("%4d", grades[i][j]);
-    }
-    printf("\n");
-  }
+  printf("$%d~%d: %d~%d\n", SIZE, SIZE + 99, reward[0], reward[99]);
 }
