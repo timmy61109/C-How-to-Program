ea72b0e8723ee99742f00c8f7491acffb1f2c36d
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-08 15:14:22 +0800

    如果採用，此提交將修改檔案位置
    修改項目:
    
    模組:
    
    議題

---
+++ src/fig09_02.c
@@ -0,0 +1,17 @@
+// Fig. 9.2: fig09_02.c
+// Using the integer conversion specifiers
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+  printf("%d\n", 445);
+}
