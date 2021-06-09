a0ea464a81535674230415de2cfb8ee0fe837f94
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-09 18:44:19 +0800

    如果採用，此提交將大致完成基本第六章作業，後續將繼續補充
    修改項目:
    
    模組:
    
    議題

--- src/test.c
+++ src/test.c
@@ -3,53 +3,13 @@
 
 
 int main(void) {
-  puts("\n(a)\n");
-  printf("%15s\n", "123456789012345");
-  printf("%.8d\n", 40000);
+  float celsius;
 
-  int x;
-  puts("\n(b)\n");
-  printf("輸入十六進制數值(ex. 0x123 or 123): ");
-  scanf("%x", &x);
-  printf("%#x\n", x);
+  printf("%10s%10s\n", "Fahrenheit", "Celsius");
+  for(float fahrenheit = 0; fahrenheit <= 212; ++fahrenheit) {
+    celsius = ((float) 5 / 9) * (fahrenheit - 32);
 
-  puts("\n(c)\n");
-  printf("%d\n", 200);
-  printf("%+d\n", 200);
+    printf("%+10.3f%+10.3f\n", fahrenheit, celsius);
 
-  puts("\n(d)\n");
-  printf("%#x\n", 256);
-
-  puts("\n(e)\n");
-  char s[15];
-  printf("%s", "Enter string: ");
-  scanf(" %15[^p]", s);
-  printf("The input was \"%s\"\n", s);
-  scanf("%[^\n]", s);
-
-  puts("\n(f)\n");
-  printf("%09g\n", 1.234);
-
-  puts("\n(g)\n");
-  int hour = 0;
-  int minute = 0;
-  int second = 0;
-  printf("%s", "Enter a date in the from hh:mm:ss : ");
-  scanf(" %d%*c%d%*c%d", &hour, &minute, &second);
-  printf("hour = %.2d minute = %.2d second = %.2d\n", hour, minute, second);
-
-  puts("\n(h)\n");
-  printf("%s", "Please input \"characters\": ");
-  scanf(" %*1[\"]%15[a-zA-Z0-9]%*1[\"]", s);
-  printf("%s\n", s);
-  scanf("%[^\n]", s);
-
-  puts("\n(i)\n");
-  hour = 0;
-  minute = 0;
-  second = 0;
-  printf("%s", "Enter a date in the from hh:mm:ss : ");
-  scanf(" %d:%d:%d", &hour, &minute, &second);
-  printf("hour = %.2d minute = %.2d second = %.2d\n", hour, minute, second);
-
+  }
 }
