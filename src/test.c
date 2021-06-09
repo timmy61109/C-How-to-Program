ec9f2c97ffaf1de22cfcae921d785ac21e824b8e
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-09 17:36:11 +0800

    如果採用，此提交將第六次作業的測試程式
    修改項目:
    
    模組:
    
    議題

--- src/test.c
+++ src/test.c
@@ -1,16 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define SIZE 6
-#define COUNT_SIZE 12
-#define CUBOID_SIZE 360
 
 
 int main(void) {
-  int y = 5;
-  int *yPtr;
+  puts("\n(a)\n");
+  printf("%15s\n", "123456789012345");
+  printf("%.8d\n", 40000);
 
-  yPtr = &y;
+  int x;
+  puts("\n(b)\n");
+  printf("輸入十六進制數值(ex. 0x123 or 123): ");
+  scanf("%x", &x);
+  printf("%#x\n", x);
 
-  printf("%d", *yPtr);
+  puts("\n(c)\n");
+  printf("%d\n", 200);
+  printf("%+d\n", 200);
+
+  puts("\n(d)\n");
+  printf("%#x\n", 256);
+
+  puts("\n(e)\n");
+  char s[15];
+  printf("%s", "Enter string: ");
+  scanf(" %15[^p]", s);
+  printf("The input was \"%s\"\n", s);
+  scanf("%[^\n]", s);
+
+  puts("\n(f)\n");
+  printf("%09g\n", 1.234);
+
+  puts("\n(g)\n");
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+  printf("%s", "Enter a date in the from hh:mm:ss : ");
+  scanf(" %d%*c%d%*c%d", &hour, &minute, &second);
+  printf("hour = %.2d minute = %.2d second = %.2d\n", hour, minute, second);
+
+  puts("\n(h)\n");
+  printf("%s", "Please input \"characters\": ");
+  scanf(" %*1[\"]%15[a-zA-Z0-9]%*1[\"]", s);
+  printf("%s\n", s);
+  scanf("%[^\n]", s);
+
+  puts("\n(i)\n");
+  hour = 0;
+  minute = 0;
+  second = 0;
+  printf("%s", "Enter a date in the from hh:mm:ss : ");
+  scanf(" %d:%d:%d", &hour, &minute, &second);
+  printf("hour = %.2d minute = %.2d second = %.2d\n", hour, minute, second);
+
 }
