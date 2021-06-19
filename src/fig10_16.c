7709b5f4f449913f2bfffe1034047f93a0c230d9
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-20 04:42:44 +0800

    如果採用，此提交將修正範例內容與更新作業內容
    修改項目:
    
    模組:
    
    議題

--- src/fig10_16.c
+++ src/fig10_16.c
@@ -31,13 +31,13 @@ void fillDeck(Card * const wDeck) {
   for (size_t i = 0; i < CARDS; ++i) {
     wDeck[i].face = i % (CARDS / 4);
     wDeck[i].suit = i / (CARDS / 4);
-    wDeck[i].suit = i / (CARDS / 2);
+    wDeck[i].color = i / (CARDS / 2);
   }
 }
 
 void deal(const Card * const wDeck) {
   printf("%-6s%-6s%-15s%-6s%-6s%s\n", "Card", "Suit", "Color", "Card", "Suit", "Color");
-  for (size_t k1 = 0, k2 = k1 + 26; k1 < CARDS; ++k1, ++k2) {
+  for (size_t k1 = 0, k2 = k1 + 26; k1 < CARDS / 2; ++k1, ++k2) {
     printf("%-6d%-6d%-15d", wDeck[k1].face, wDeck[k1].suit, wDeck[k1].color);
     printf("%-6d%-6d%-15d\n", wDeck[k1].face, wDeck[k1].suit, wDeck[k1].color);
   }
--- src/test.c
+++ src/test.c
@@ -1,15 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+#define CARDS 52
+#define FACES 13
+
+struct bitCard {
+  unsigned int face : 4;
+  unsigned int suit : 2;
+};
+
+typedef struct bitCard Card;
+
+void fillDeck(Card * const wDeck);
+void shuffle(Card * const wDeck);
+void deal(const Card * const wDeck);
 
 int main(void) {
-  float celsius;
+  Card deck[CARDS];
+  srand(time(NULL));
+  fillDeck(deck);
+  shuffle(deck);
+  deal(deck);
+}
 
-  printf("%10s%10s\n", "Fahrenheit", "Celsius");
-  for(float fahrenheit = 0; fahrenheit <= 212; ++fahrenheit) {
-    celsius = 5.0 / 9.0 * (fahrenheit - 32);
+void fillDeck(Card * const wDeck) {
+  for (size_t i = 0; i < CARDS; ++i) {
+    wDeck[i].face = i % (CARDS / 4);
+    wDeck[i].suit = i / (CARDS / 4);
+  }
+}
 
-    printf("%+10.3f%+10.3f\n", fahrenheit, celsius);
+void shuffle(Card * const wDeck) {
+  for (size_t i = 0; i < CARDS; ++i) {
+    size_t j = rand() % CARDS;
+    Card temp = wDeck[i];
+    wDeck[i] = wDeck[j];
+    wDeck[j] = temp;
+  }
+}
 
+void deal(const Card * const wDeck) {
+  char face[13][10] = {
+    "Ace", "Deuce", "Three", "Four",
+    "Five", "Six", "Seven", "Eight",
+    "Nine", "Ten", "Jack", "Queen", "King"
+  };
+  char suit[4][10] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+  printf("%-10s%-15s%-10s%-10s\n", "Suit", "Card", "Suit", "Card");
+  for (size_t k1 = 0, k2 = k1 + 26; k1 < CARDS / 2; ++k1, ++k2) {
+    printf("%-10s%-15s", suit[wDeck[k1].suit], face[wDeck[k1].face]);
+    printf("%-10s%-10s\n", suit[wDeck[k2].suit], face[wDeck[k2].face]);
   }
 }
