283858775f788f0790ef28eebb295c7a6f8a77b1
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-02 13:31:54 +0800

    此提交將新增物件導向的變數設定
    修改項目:
    
    模組:
    
    議題

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 filename='test'
 chapter='05' && section='03' && filename=fig${chapter}_${section}
+chapter='11' && section='0' && filename=ch${chapter}_self${section}
 filename=fig${chapter}_${section}
 g++ $filename.c -o $filename.o && ./$filename.o
