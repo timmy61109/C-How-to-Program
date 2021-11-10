3a21b12b740cd7b684e1f012114644c568953cde
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-10 23:55:34 +0800

    此提交將加入程式範例
    修改項目:
    
    模組:
    
    議題

---
+++ src/fig11_06.c
@@ -0,0 +1,24 @@
+// Fig. 11.2: fig11_02.c
+// Creating a sequential file
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  FILE *stderr_p;
+
+  if ((stderr_p = fopen("clients.txt", "r")) == NULL) {
+    puts("File could not be opened");
+  } else {
+    unsigned int account = 0;
+    char name[256] = {};
+    double balance = 0;
+
+    printf("%-10s%-13s%s\n", "Account", "Name", "Balance");
+    fscanf(stderr_p, "%d%29s%lf", &account, name, &balance);
+
+    while (!feof(stderr_p)) {
+      printf("%-10d%-13s%7.2f\n", account, name, balance);
+      fscanf(stderr_p, "%d%29s%lf", &account, name, &balance);
+    }
+    fclose(stderr_p);
+  }
+}
