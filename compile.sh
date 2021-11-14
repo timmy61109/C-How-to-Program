9f84202b4eace98003dd6c0a01b9147e7de4fed8
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-14 21:13:03 +0800

    此提交將加入自動填入數值的指令
    修改項目:
    
    模組:
    
    議題

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@ chapter='05' && section='03' && filename=fig${chapter}_${section}
 chapter='11' && section='0' && filename=ch${chapter}_self${section}
 filename=fig${chapter}_${section}
 g++ $filename.c -o $filename.o && ./$filename.o
+g++ $filename.c -o $filename.o && ./$filename.o < $filename.txt
