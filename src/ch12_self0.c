27b34d8cffc7b8a36d7420d3ac58c7fdb314f97b
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-16 18:21:35 +0800

    此提交將修改與新增函式，包括改用陣列方式儲存字元，新增資料處理函式
    修改項目:
    
    模組:
    
    議題

--- src/ch12_self0.c
+++ src/ch12_self0.c
@@ -12,88 +12,97 @@
 #define MONEY 22
 
 typedef struct {
-  char *license_plate = (char*) "";
-  char *engine_number = (char*) "";
-  char *name = (char*) "";
-  char *id = (char*) "";
-  char *phone_number = (char*) "";
-  char *address = (char*) "";
-  char *date = (char*) "";
-  char *amount_of_money = (char*) "";
+  unsigned int number;
+  char license_plate[LICENSE_PLATE];
+  unsigned int engine_number;
+  char name[NAME];
+  char id[ID];
+  char phone_number[PHONE_NUMBER];
+  char address[ADDR];
+  char date[DATE];
+  int amount_of_money;
 } vehicle_management_t;
 
-void print_data(vehicle_management_t *data);
+void print_data(vehicle_management_t *data_p, unsigned int count);
 void print_row_data(vehicle_management_t data);
-void search(vehicle_management_t *data);
 void example_data(vehicle_management_t *data);
-void input_data(vehicle_management_t *data);
-void write_data(vehicle_management_t *data_p, size_t count);
-void write_row_data(vehicle_management_t data);
+void keyin(vehicle_management_t *data, unsigned int count);
+void search(vehicle_management_t *data);
+void write_data(char *name, vehicle_management_t *data_p, unsigned int count);
+void write_row_data(char *name, vehicle_management_t data);
+void read_row_data();
+void export_csv(char *name, vehicle_management_t *data_p, unsigned int count);
+void creat_database(char *name, unsigned int count);
+void insert(FILE * file_p, unsigned int count, vehicle_management_t *data, char *name);
+void selete();
+void delete_form(FILE * file_p, unsigned int count);
+void update(FILE * file_p, unsigned int count);
 
 int main() {
   vehicle_management_t in_data[SIZE];
-  vehicle_management_t example[SIZE];
+  char database_p[23] = "vehicle_management.dat";
 
-  example_data(example);
-  printf("%s\n\n", "The is Example.");
-  print_data(example);
-  input_data(in_data);
-  printf("\n%s\n\n", "The is key in data.");
-  print_data(in_data);
-  write_data(in_data, SIZE);
-
+  creat_database(database_p, 10000);
   search(in_data);
 
 }
 
-void print_data(vehicle_management_t *data_p) {
-  for (size_t i = 0; i < SIZE; i++) {
+void print_data(vehicle_management_t *data_p, unsigned int count) {
+  printf("%-10s%-22s%-15s%-10s%-12s%-17s%-64s%-22s%-20s\n",
+    "number", "license plate", "engine number", "name", "ID",
+    "phone number", "address", "datetime", "amount of money");
+  for (size_t i = 0; i < count; i++) {
     printf("%p\n", &*(data_p + i));
     print_row_data(*(data_p + i));
   }
 }
 
 void print_row_data(vehicle_management_t data) {
-  printf("%s%s\n", "車牌：", data.license_plate);
-  printf("%s%s\n", "引擎編號：", data.engine_number);
-  printf("%s%s\n", "姓名：", data.name);
-  printf("%s%s\n", "身份證字號：", data.id);
-  printf("%s%s\n", "電話：", data.phone_number);
-  printf("%s%s\n", "地址：", data.address);
-  printf("%s%s\n", "時間：", data.date);
-  printf("%s%s\n\n", "租金：", data.amount_of_money);
-
+  printf("%-10u%-22s%-15u%-10s%-12s%-17s%-64s%-22s%-20u\n",
+    data.number,
+    data.license_plate,
+    data.engine_number,
+    data.name,
+    data.id,
+    data.phone_number,
+    data.address,
+    data.date,
+    data.amount_of_money
+  );
 }
 
 void example_data(vehicle_management_t *data) {
   vehicle_management_t create[SIZE] = {
     {
-      (char*) "2432-6962-0262-2139",
-      (char*) "21086155",
-      (char*) "甯庭佳",
-      (char*) "J257823365",
-      (char*) "+886 976041644",
-      (char*) "457 新北市雙溪區大灣七街二段335巷144弄740號13樓",
-      (char*) "Februaunsigned int *account, char *name, double *balancery 18, 1988",
-      (char*) "73200"
+      1,
+      "2432-6962-0262-2139",
+      21086155,
+      "甯庭佳",
+      "J257823365",
+      "+886 976041644",
+      "457 新北市雙溪區大灣七街二段335巷144弄740號13樓",
+      "Februa 18, 1988",
+      73200
     }, {
-      (char*) "5295-7162-0442-0867",
-      (char*) "31312262",
-      (char*) "司婉安",
-      (char*) "Q214131854",
-      (char*) "+886 923402560",
-      (char*) "374-80 基隆市中正區通明街六段553巷966弄277號44樓",
-      (char*) "July 4, 1936",
-      (char*) "65800"
+      2,
+      "5295-7162-0442-0867",
+      31312262,
+      "司婉安",
+      "Q214131854",
+      "+886 923402560",
+      "374-80 基隆市中正區通明街六段553巷966弄277號44樓",
+      "July 4, 1936",
+      65800
     }, {
-      (char*) "2432-6962-0262-2139",
-      (char*) "69848125",
-      (char*) "姜雅涵",
-      (char*) "E129441704",
-      (char*) "+886 959404352",
-      (char*) "670 高雄市左營區吉利四街350號",
-      (char*) "February 2, 1999",
-      (char*) "114900"
+      3,
+      "2432-6962-0262-2139",
+      69848125,
+      "姜雅涵",
+      "E129441704",
+      "+886 959404352",
+      "670 高雄市左營區吉利四街350號",
+      "February 2, 1999",
+      114900
     }
   };
   for (size_t i = 0; i < SIZE; i++) {
@@ -101,49 +110,35 @@ void example_data(vehicle_management_t *data) {
   }
 }
 
-void input_data(vehicle_management_t *data) {
-  char license_plate[SIZE][LICENSE_PLATE];
-  char engine_number[SIZE][ENGINE];
-  char name[SIZE][NAME];
-  char id[SIZE][ID];
-  char phone_number[SIZE][PHONE_NUMBER];
-  char address[SIZE][ADDR];
-  char date[SIZE][DATE];
-  char amount_of_money[SIZE][MONEY];
+void keyin(vehicle_management_t *data, unsigned int count) {
 
-  for (size_t i = 0; i < SIZE; i++) {
+  for (size_t i = 0; i < count; i++) {
+    printf("%s", "\n輸入編號：");
+    scanf(" %u", &(data + i)->number);
+
     printf("%s", "輸入車牌：");
-    scanf(" %[^\n]", license_plate[i]);
+    scanf(" %[^\n]", (data + i)->license_plate);
 
     printf("%s", "輸入引擎編號：");
-    scanf(" %[^\n]", engine_number[i]);
+    scanf(" %u", &(data + i)->engine_number);
 
     printf("%s", "輸入姓名：");
-    scanf(" %[^\n]", name[i]);
+    scanf(" %[^\n]", (data + i)->name);
 
     printf("%s", "輸入身份證字號：");
-    scanf(" %[^\n]", id[i]);
+    scanf(" %[^\n]", (data + i)->id);
 
     printf("%s", "輸入電話：");
-    scanf(" %[^\n]", phone_number[i]);
+    scanf(" %[^\n]", (data + i)->phone_number);
 
     printf("%s", "輸入地址：");
-    scanf(" %[^\n]", address[i]);
+    scanf(" %[^\n]", (data + i)->address);
 
     printf("%s", "輸入時間：");
-    scanf(" %[^\n]", date[i]);
+    scanf(" %[^\n]", (data + i)->date);
 
     printf("%s", "輸入租金：");
-    scanf(" %[^\n]", amount_of_money[i]);
-
-    (data + i)->license_plate = license_plate[i];
-    (data + i)->engine_number = engine_number[i];
-    (data + i)->name = name[i];
-    (data + i)->id = id[i];
-    (data + i)->phone_number = phone_number[i];
-    (data + i)->address = address[i];
-    (data + i)->date = date[i];
-    (data + i)->amount_of_money = amount_of_money[i];
+    scanf(" %u", &(data + i)->amount_of_money);
 
   }
 }
@@ -167,30 +162,22 @@ void search(vehicle_management_t *data) {
   }
 }
 
-void keyin(vehicle_management_t *data, unsigned int *account, char *name, double *balance) {
-  puts("Enter the account, name, and balance.");
-  puts("Enter EOF to end input.(Linux is Ctrl + D, Windows is Ctrl + Z)");
-  while (!feof(stdin)) {
-    printf("%s", "> ");
-    scanf("%d%29s%lf[^\n]", account, name, balance);
-  }
-}
-
-void write_data(vehicle_management_t *data_p, size_t count) {
+void write_data(char *name, vehicle_management_t *data_p, unsigned int count) {
   puts("File is write...");
   for (size_t i = 0; i < count; i++) {
     printf("%p row: %ld\n", &*(data_p + i), i);
-    write_row_data(*(data_p + i));
+    write_row_data(name, *(data_p + i));
   }
 }
 
-void write_row_data(vehicle_management_t data) {
+void write_row_data(char *name, vehicle_management_t data) {
   FILE *stderr_p;
 
-  if ((stderr_p = fopen("vehicle_management_data.csv", "a")) == NULL) {
+  if ((stderr_p = fopen(name, "a")) == NULL) {
     puts("File could not be opened");
   } else {
-    fprintf(stderr_p, "%s, %s, %s, %s, %s, %s, %s, %s\n",
+    fprintf(stderr_p, "%u, %s, %u, %s, %s, %s, %s, %s, %u\n",
+      data.number,
       data.license_plate,
       data.engine_number,
       data.name,
@@ -204,6 +191,142 @@ void write_row_data(vehicle_management_t data) {
   }
 }
 
-void read_row_data() {
+void read_row_data(char *name, vehicle_management_t *data_p, unsigned int count) {
+  FILE *cf_p;
+
+  if ((cf_p = fopen(name, "r")) == NULL) {
+    puts("File could not be opened");
+  } else {
+    fscanf(cf_p, "%u%s%u%s%s%s%s%s%d",
+      &data_p->number,
+      data_p->license_plate,
+      &data_p->engine_number,
+      data_p->name,
+      data_p->id,
+      data_p->phone_number,
+      data_p->address,
+      data_p->date,
+      &data_p->amount_of_money
+    );
+    data_p++;
+
+    while (!feof(cf_p)) {
+      fscanf(cf_p, "%u%s%u%s%s%s%s%s%d",
+        &data_p->number,
+        data_p->license_plate,
+        &data_p->engine_number,
+        data_p->name,
+        data_p->id,
+        data_p->phone_number,
+        data_p->address,
+        data_p->date,
+        &data_p->amount_of_money
+      );
+      data_p++;
+    }
+    fclose(cf_p);
+  }
+
+}
+
+void export_csv(char *name, vehicle_management_t *data_p, unsigned int count) {
+  write_data(name, data_p, count);
+}
+
+void creat_database(char *name, unsigned int count) {
+  FILE *cf_p;
+  if ((cf_p = fopen(name, "wb")) == NULL) {
+    puts("File could not be opened.");
+
+  } else {
+    vehicle_management_t database;
+
+    for (size_t i = 0; i <= count; i++) {
+      fwrite(&database, sizeof(vehicle_management_t), 1, cf_p);
+    }
+
+    fclose(cf_p);
+  }
+}
+
+void insert(FILE * file_p, unsigned int count, vehicle_management_t *data, char *name) {
+  puts("Enter the account, name, and balance.");
+  puts("Enter EOF to end input.(Linux is Ctrl + D, Windows is Ctrl + Z)");
+  while (!feof(stdin)) {
+    printf("%s", "> ");
+    printf("%s%u%s", "Enter account to update (1 - ", count, "): ");
+    unsigned int account;
+    scanf("%d", &account);
+    fseek(file_p, (account - 1) * sizeof(vehicle_management_t), SEEK_SET);
+
+    fread(data, sizeof(vehicle_management_t), 1, file_p);
+
+    if (data->number != 0) {
+      printf("Account #%u already contains information.\n", data->number);
+
+    } else {
+      keyin(data, count);
+
+      fseek(file_p, (data->number - 1) * sizeof(vehicle_management_t), SEEK_SET);
+      fwrite(&data, sizeof(vehicle_management_t), 1, file_p);
+    }
+  }
+
+}
+
+void selete() {
+}
+
+void delete_form(FILE * file_p, unsigned int count) {
+  printf("%s%u%s", "Enter account to delete (1 - ", count, "): ");
+  unsigned int account = 0;
+  scanf("%d", &account);
+
+  fseek(file_p, (account - 1) * sizeof(vehicle_management_t), SEEK_SET);
+
+  vehicle_management_t data;
+
+  fread(&data, sizeof(vehicle_management_t), 1, file_p);
+
+  if (data.number == 0) {
+    printf("Account %d does no exist.\n", account);
+
+  } else {
+    fseek(file_p, (account - 1) * sizeof(vehicle_management_t), SEEK_SET);
+    vehicle_management_t data;
+    fwrite(&data, sizeof(vehicle_management_t), 1, file_p);
+
+    puts("Delete success");
+  }
+}
+
+void update(FILE * file_p, unsigned int count) {
+  printf("%s%u%s", "Enter account to update (1 - ", count, "): ");
+  unsigned int account = 0;
+  scanf("%d", &account);
+  fseek(file_p, (account - 1) * sizeof(vehicle_management_t), SEEK_SET);
+
+  vehicle_management_t data;
+  fread(&data, sizeof(vehicle_management_t), 1, file_p);
+
+  if (data.number == 0) {
+    printf("Account #%d has no information.\n", account);
+
+  } else {
+    print_row_data(data);
+
+    printf("%s", "Enter charge (+) or payment (-): ");
+    double transaction;
+    scanf("%lf", &transaction);
+    data.amount_of_money += transaction;
+
+    print_row_data(data);
+
+    fseek(file_p, (account - 1) * sizeof(vehicle_management_t), SEEK_SET);
+    fwrite(&data, sizeof(vehicle_management_t), 1, file_p);
+  }
+}
+
+void merge() {
 
 }
