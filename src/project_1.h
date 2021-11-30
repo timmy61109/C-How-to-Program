b9fffbff37187272d9778d1cbfbd897d310a90fe
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-11-30 20:21:33 +0800

    此提交將調整縮排
    修改項目:
    
    模組:
    
    議題

--- src/project_1.h
+++ src/project_1.h
@@ -1,14 +1,14 @@
 // 宣告複數類別
 class Complex {
   private:
-	float real,img;
+    float real,img;
   public:
-	Complex(float r,float i);  // Constructor
-	Complex() {}  // Default Constructor
-  ~Complex() {}  // Destructor
-	Complex& add(const Complex& c,const Complex& d);  // 相加函數
-	Complex& sub(const Complex& c,const Complex& d);  // 相減函數
-	Complex& mul(const Complex& c,const Complex& d);  // 相乘函數
-	Complex& div(const Complex& c,const Complex& d);  // 相除函數
-	void showdata();
+    Complex(float r,float i);  // Constructor
+  	Complex() {}  // Default Constructor
+    ~Complex() {}  // Destructor
+  	Complex& add(const Complex& c,const Complex& d);  // 相加函數
+  	Complex& sub(const Complex& c,const Complex& d);  // 相減函數
+  	Complex& mul(const Complex& c,const Complex& d);  // 相乘函數
+  	Complex& div(const Complex& c,const Complex& d);  // 相除函數
+  	void showdata();
 };
