dbe3cd011c5d9a2be4e19016fed8356c8dd3e23d
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-12-02 07:39:40 +0800

    此提交將修正資料大小，避免記憶體溢位
    修改項目:
    
    模組:
    
    議題

--- src/ch12_self0.c
+++ src/ch12_self0.c
@@ -6,13 +6,13 @@ vehicle management
 #include <stdlib.h>
 #include <string.h>
 #define SIZE 3
-#define DATABASE 10000
+#define DATABASE 300
 #define LICENSE_PLATE 22
 #define ENGINE 10
 #define NAME 22
 #define ID 12
 #define PHONE_NUMBER 17
-#define ADDR 2000 * 32
+#define ADDR 6400
 #define DATE 22
 #define MONEY 22
 
