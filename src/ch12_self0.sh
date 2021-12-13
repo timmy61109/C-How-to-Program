68dabb3f4d88d65ba0b0a93e1e27f3f6f2a78c0f
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-12-13 12:44:43 +0800

    此提交將改名為cpp
    修改項目:
    
    模組:
    
    議題

--- src/ch12_self0.sh
+++ src/ch12_self0.sh
@@ -1,2 +1,2 @@
 chapter='12' && section='0' && filename=ch${chapter}_self${section}
-g++ $filename.c -o $filename.o && ./$filename.o < $filename.txt
+g++ $filename.cpp -o $filename.o && ./$filename.o < $filename.txt
