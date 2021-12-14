8d2e8c1584ab640953918a0f09b1d35f9a5b2b8c
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-12-14 21:00:18 +0800

    此提交將新增老師的作業與範例
    修改項目:
    
    模組:
    
    議題

--- src/student.cpp
+++ src/student.cpp
@@ -1,73 +1,67 @@
 #include <iostream>
-#include <stdio.h>
 #include <string.h>
 
+// 類別宣告
 class Student {
   private:
-    char *school;  // 學校
-    char *class_num;  // 班級
-    char name[8];  // 姓名
-    long student_num;  // 學號
+    char *name_p;
+    char *school_num_p;
+    int age;
+    static int total_student;  // 記錄學生總數
   public:
-    Student(char *s, char *c);  // Constructor 宣告
-    Student(const Student& str);  // Copy constructor宣告
-    Student() {}  // default Constructor
+    Student(char *name, char *num, int count);  // Constructor
+    Student() {}  // default constructor
     ~Student() {
       // Destructor
-      delete school;
-      delete class_num;
+      delete name_p, school_num_p;
     }
-    void ip_data();
-    void show_data();
+
+    static int get_num() {
+      return total_student;
+    }
+    void show();  // 顯示函數
 };
 
-Student::Student(char *s, char *c) {
-  unsigned len;
-  len = strlen(s);  // 複製學校名
-  school = new char[len + 1];
-  strcpy(school, s);
-  len = strlen(c);  // 複製班級名
-  class_num = new char[len + 1];
-  strcpy(class_num, c);
+Student::Student(char *name, char *num, int count) {
+  // Constructor
+  name_p = new char[strlen(name) + 1];
+  school_num_p = new char[strlen(num) + 1];
+  strcpy(name_p, name);
+  strcpy(school_num_p, num);
+  age = count;
+  total_student += 1;
 }
 
-Student::Student(const Student& str) {
-  // Copy constructor
-  unsigned len;
-  len = strlen(str.school);  //複製學校名
-  school = new char[len+1];
-  strcpy(school,str.school);
-  len = strlen(str.class_num);  //複製班級名
-  class_num = new char[len+1];
-  strcpy(class_num,str.class_num);
- }
-
-void Student::ip_data() {
-  std::cout << "\n姓名=";
-  std::cin >> name;
-  std::cout << "學號=";
-  std::cin >> student_num;
+void Student::show() {
+  // 資料顯示
+  std::cout << "\n\n姓名=" << name_p << "\n學號=" << school_num_p << "\n年齡=" << age;
 }
 
-void Student::show_data() {
-  std::cout << "\n學校=" << school
-  << "\t班級=" << class_num
-	<< "\t姓名=" << name
-	<< "\t學號=" << student_num
-  << "\n\n";
-}
+int Student::total_student = 0;
 
+// 主程式
 int main() {
-  char school[256] = "國立金門技術學院";
-  char cls[256] = "電三甲";
-  Student stu1(school, cls);
-  Student stu2 = stu1;
-  Student stu3 = stu1;
-  stu1.ip_data();
-  stu2.ip_data();
-  stu3.ip_data();
-  stu1.show_data();
-  stu2.show_data();
-  stu3.show_data();
-  return 0;
+  // 物件定義，並定義初值
+  char school_num[256];
+  char name = "Losa";
+  school_num = "1234224";
+  Student student[4] = {
+    Student(&name, school_num, 11),
+    Student("John", "1234424", 14),
+    Student("Jane", "1216224", 13),
+    Student("Mary", "3224224", 12)
+  };
+
+  Student student_temp[4];
+
+  for (size_t i = 0; i < Student::get_num(); i++) {
+
+  }
+
+  // 顯示
+  for (int i = 0; i< Student::get_num(); i++) {
+    student[i].show();
+    std::cout << "\n";
+  }
+	return 0;
 }
