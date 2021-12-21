ffb57ca2782c48ae2e6767b391d206a7d141bab7
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-12-21 18:58:16 +0800

    此提交將修改結構，使內容可以正常運行
    修改項目:
    
    模組:
    
    議題

--- src/student.cpp
+++ src/student.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
 #include <string.h>
+#ifndef SIZE
+#define SIZE 50
+#endif
 
 // 類別宣告
 class Student {
   private:
-    char *name_p;
-    char *school_num_p;
+    char name[SIZE];
+    char school_num[SIZE];
     int age;
     static int total_student;  // 記錄學生總數
   public:
-    Student(char *name, char *num, int count);  // Constructor
+    Student(char *name_p, char *school_num_p, int number_p);  // Constructor
     Student() {}  // default constructor
     ~Student() {
       // Destructor
-      delete name_p, school_num_p;
+      delete name, school_num;
     }
 
     static int get_num() {
       return total_student;
     }
-    void show();  // 顯示函數
+    void selection_sort();
+    void show();
 };
 
-Student::Student(char *name, char *num, int count) {
+Student::Student(char *name_p, char *school_num_p, int number_p) {
   // Constructor
-  name_p = new char[strlen(name) + 1];
-  school_num_p = new char[strlen(num) + 1];
-  strcpy(name_p, name);
-  strcpy(school_num_p, num);
-  age = count;
+  for (size_t i = 0; i < SIZE; i++) {
+    name_p[i] = name[i];
+    school_num_p[i] = school_num[i];
+  }
+  age = number_p;
   total_student += 1;
 }
 
 void Student::show() {
   // 資料顯示
-  std::cout << "\n\n姓名=" << name_p << "\n學號=" << school_num_p << "\n年齡=" << age;
+  std::cout << "\n\n姓名=" << name << "\n學號=" << school_num << "\n年齡=" << age;
+}
+
+void Student::selection_sort(/* arguments */) {
+  Student student_temp[4];
+
+  for (size_t i = 0; i < (size_t) Student::get_num(); i++) {
+
+  }
+
 }
 
 int Student::total_student = 0;
@@ -42,21 +55,12 @@ int Student::total_student = 0;
 // 主程式
 int main() {
   // 物件定義，並定義初值
-  char school_num[256];
-  char name = "Losa";
-  school_num = "1234224";
   Student student[4] = {
-    Student(&name, school_num, 11),
+    Student("Losa", "1234224", 11),
     Student("John", "1234424", 14),
     Student("Jane", "1216224", 13),
     Student("Mary", "3224224", 12)
   };
-
-  Student student_temp[4];
-
-  for (size_t i = 0; i < Student::get_num(); i++) {
-
-  }
 
   // 顯示
   for (int i = 0; i< Student::get_num(); i++) {
