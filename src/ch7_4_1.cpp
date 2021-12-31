1c2cf3d0c20d939488796c995647559078b3160f
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-12-31 21:29:46 +0800

    此提交將風格化
    修改項目:
    
    模組:
    
    議題

--- src/ch7_4_1.cpp
+++ src/ch7_4_1.cpp
@@ -4,91 +4,93 @@
 
 
 Common::Common(int c, int e, int m) {
- Chinese=c;
- English=e;
- Math=m;
-}
-void Common::showdata()  // 共同科目資料成員的顯示
-{
- std::cout <<"\n中文成績-->" << Chinese<<"\n英文成績-->"
-         << English<<"\n數學成績-->" << Math;
-}
-
-Electronics::Electronics(int com, int cir)
-{
- Computer=com;
- Circuit=cir;
+  Chinese = c;
+  English = e;
+  Math = m;
 }
 
 
-void Electronics::showdata() // 電子科目資料成員的顯示
-{
- std::cout << "\n電腦概論-->" << Computer<< "\n電路學-->"  << Circuit;
-}
-Account::Account(int eco,int acc)
-{
- Economics=eco;
- Accounting=acc;
+// 共同科目資料成員的顯示
+void Common::showdata() {
+  std::cout << "\n中文成績-->" << Chinese << "\n英文成績-->" << English << "\n數學成績-->" << Math;
 }
 
 
-void Account::showdata()         // 會計科目資料成員的顯示
-{
+Electronics::Electronics(int com, int cir) {
+ Computer = com;
+ Circuit = cir;
+}
+
+
+// 電子科目資料成員的顯示
+void Electronics::showdata() {
+  std::cout << "\n電腦概論-->" << Computer<< "\n電路學-->"  << Circuit;
+}
+
+
+Account::Account(int eco, int acc) {
+  Economics = eco;
+  Accounting = acc;
+}
+
+
+// 會計科目資料成員的顯示
+void Account::showdata() {
    std::cout << "\n經濟學-->" << Economics<< "\n會計學-->"  << Accounting;
-}										                // Constructor，成員初設串列
+}  // Constructor，成員初設串列
 
 
-Mechanics::Mechanics(int mat, int con)
-{
- Material=mat;
- Control=con;
+Mechanics::Mechanics(int mat, int con) {
+  Material = mat;
+  Control = con;
 }
 
 
-void Mechanics::showdata()         // 機械科目資料成員的顯示
-{
+// 機械科目資料成員的顯示
+void Mechanics::showdata() {
    std::cout << "\n經濟學-->" << Material << "\n會計學-->"  << Control;
 }
+
+
 // Constructor，成員初設串列
-
-
-Student_EE::Student_EE(char *n,char *s, int chi,int eng,int math,
-  int com, int cir) :Common(chi,eng,math),Electronics(com,cir)
- {
-  strcpy(name,n);                  // 學生資料成員的設定
+Student_EE::Student_EE(char *n, char *s, int chi, int eng, int math, int com, int cir):Common(chi, eng, math), Electronics(com,cir) {
+  strcpy(name,n);  // 學生資料成員的設定
   strcpy(stu_num,s);
- }
-void Student_EE::showdata( )
-{                                                         // 學生資料成員的顯示
- std::cout << "\n學生姓名-->" << name << "\n學號-->"  << stu_num;
- Common::showdata();                     // 共同科目資料成員的顯示 Electronics::showdata();                // 電子科目資料成員的顯示
 }
+
+
+// 共同科目資料成員的顯示 Electronics::showdata();  // 電子科目資料成員的顯示
+void Student_EE::showdata( ) {  // 學生資料成員的顯示
+  std::cout << "\n學生姓名-->" << name << "\n學號-->"  << stu_num;
+  Common::showdata();
+}
+
 
 // Constructor，成員初設串列
 Student_AC::Student_AC(char *n,char *s, int chi,int eng,int math,int eco, int acc) :Common(chi,eng,math),Account(eco,acc) {
-  strcpy(name,n);               // 學生資料成員的設定
+  strcpy(name,n);  // 學生資料成員的設定
   strcpy(stu_num,s);
 }
 
 
-void Student_AC::showdata()
-{                                        // 學生資料成員的顯示
-     std::cout << "\n學生姓名-->" << name<< "\n學號-->"  << stu_num;
-     Common::showdata();    // 共同科目資料成員的顯示
+// 學生資料成員的顯示
+void Student_AC::showdata() {
+     std::cout << "\n學生姓名-->" << name << "\n學號-->"  << stu_num;
+     Common::showdata();  // 共同科目資料成員的顯示
      Account::showdata();  // 電子科目資料成員的顯示
 }
 
 
 // Constructor，成員初設串列
 Student_ME::Student_ME(char *n, char *s, int chi, int eng, int math, int mat, int con):Common(chi, eng, math), Mechanics(mat, con) {
-  strcpy(name, n);               // 學生資料成員的設定
+  strcpy(name, n);  // 學生資料成員的設定
   strcpy(stu_num, s);
 }
 
 
-void Student_ME::showdata()
-{                                        // 學生資料成員的顯示
-     std::cout << "\n學生姓名-->" << name<< "\n學號-->"  << stu_num;
-     Common::showdata();    // 共同科目資料成員的顯示
-     Mechanics::showdata();  // 電子科目資料成員的顯示
+// 學生資料成員的顯示
+void Student_ME::showdata() {
+  std::cout << "\n學生姓名-->" << name<< "\n學號-->"  << stu_num;
+  Common::showdata();  // 共同科目資料成員的顯示
+  Mechanics::showdata();  // 電子科目資料成員的顯示
 }
