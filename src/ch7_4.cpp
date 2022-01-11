dcac43cd9b551237fb59edb46919a784248b97f0
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-01-11 15:56:13 +0800

    此提交將更新風格
    修改項目:
    
    模組:
    
    議題

--- src/ch7_4.cpp
+++ src/ch7_4.cpp
@@ -3,51 +3,9 @@
 
 
 int main() {
-  student_t student_1 = {
-    98,
-    89,
-    79,
-    99,
-    96,
-    0,
-    0,
-    0,
-    0,
-    "陳彥良",
-    "801012",
-    "電子科",
-    "asdf@gmail.com",
-  };
-  student_t student_2 = {
-    98,
-    77,
-    69,
-    0,
-    0,
-    66,
-    80,
-    0,
-    0,
-    "余河天",
-    "801013",
-    "會計科",
-    "dddd@gmail.com",
-  };
-  student_t student_3 = {
-    77,
-    55,
-    44,
-    0,
-    0,
-    0,
-    0,
-    87,
-    88,
-    "吳小東",
-    "801014",
-    "機械科",
-    "mechanics@gmail.com",
-  };
+  student_t student_1 = {"陳彥良", "801012", "電子科", "asdf@electronics.com", 98, 89, 79, 99, 96, 0, 0, 0, 0};
+  student_t student_2 = {"余河天", "801013", "會計科", "dddd@gmail.com", 98, 77, 69, 0, 0, 66, 80, 0, 0};
+  student_t student_3 = {"吳小東", "801014", "機械科", "mechanics@gmail.com", 77, 55, 44, 0, 0, 0, 0, 87, 88};
   Colloge student1(student_1);
   Colloge student2(student_2);
   Colloge student3(student_3);
